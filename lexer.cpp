1a1b0f61d0a8dd5e0aa76d4310a273f25a4c6d82
kuetan <kue.gamer@gmail.com>
2018-11-10 02:44:21 +0900

    update

--- lexer.cpp
+++ lexer.cpp
@@ -19,49 +19,121 @@ static int gettok() {
   while (isspace(LastChar)){
     LastChar = getchar();
   }
-}
-
-// identifier: [a-zA-Z][a-zA-Z0-9]*
-if (isalpha(LastChar)) {
-  IdentifierStr = LastChar;
-  while (isalnum((LastChar = getchar()))) {
-    IdentifierStr += LastChar;
+  // identifier: [a-zA-Z][a-zA-Z0-9]*
+  if (isalpha(LastChar)) {
+    IdentifierStr = LastChar;
+    while (isalnum((LastChar = getchar()))) {
+      IdentifierStr += LastChar;
+    }
+    if (IdentifierStr == "def") {
+      return tok_def;
+    }
+    if (IdentifierStr == "extern") {
+      return tok_extern;
+    }
+    return tok_identifier;
   }
-  if (IdentifierStr == "def") {
-    return tok_def;
-  }
-  if (IdentifierStr == "extern") {
-    return tok_extern;
-  }
-  return tok_identifier;
- }
 
-if (isdigit(LastChar) || LastChar == '.') {   // Number: [0-9.]+
-  std::string NumStr;
-  do {
-    NumStr += LastChar;
-    LastChar = getchar();
-  } while (isdigit(LastChar) || LastChar == '.');
-  NumVal = strtod(NumStr.c_str(), 0);
-  return tok_number;
- }
+  if (isdigit(LastChar) || LastChar == '.') {   // Number: [0-9.]+
+    std::string NumStr;
+    do {
+      NumStr += LastChar;
+      LastChar = getchar();
+    } while (isdigit(LastChar) || LastChar == '.');
+    NumVal = strtod(NumStr.c_str(), 0);
+    return tok_number;
+  }
 
-if (LastChar == '#') {
-  // Comment until end of line.
+  if (LastChar == '#') {
+    // Comment until end of line.
     do
       LastChar = getchar();
     while (LastChar != EOF && LastChar != '\n' && LastChar != '\r');
 
     if (LastChar != EOF)
       return gettok();
- }
+  }
 
-// Check for end of file.  Don't eat the EOF.
-if (LastChar == EOF)
-  return tok_eof;
+  // Check for end of file.  Don't eat the EOF.
+  if (LastChar == EOF)
+    return tok_eof;
 
-// Otherwise, just return the character as its ascii value.
-int ThisChar = LastChar;
-LastChar = getchar();
-return ThisChar;
+  // Otherwise, just return the character as its ascii value.
+  int ThisChar = LastChar;
+  LastChar = getchar();
+  return ThisChar;
+}
+
+/// ExprAST - Base class for all expression nodes.
+class ExprAST {
+public:
+  virtual ~ExprAST() {}
+};
+
+/// NumberExprAST - Expression class for numeric literals like "1.0".
+class NumberExprAST : public ExprAST {
+  double Val;
+
+public:
+  NumberExprAST(double Val) : Val(Val) {}
+};
+
+/// VariableExprAST - Expression class for referencing a variable, like "a".
+class VariableExprAST : public ExprAST {
+  std::string Name;
+
+public:
+  VariableExprAST(const std::string &Name) : Name(Name) {}
+};
+
+/// BinaryExprAST - Expression class for a binary operator.
+class BinaryExprAST : public ExprAST {
+  char Op;
+  std::unique_ptr<ExprAST> LHS, RHS;
+
+public:
+  BinaryExprAST(char op, std::unique_ptr<ExprAST> LHS,
+                std::unique_ptr<ExprAST> RHS)
+    : Op(op), LHS(std::move(LHS)), RHS(std::move(RHS)) {}
+};
+
+/// CallExprAST - Expression class for function calls.
+class CallExprAST : public ExprAST {
+  std::string Callee;
+  std::vector<std::unique_ptr<ExprAST>> Args;
+
+public:
+  CallExprAST(const std::string &Callee,
+              std::vector<std::unique_ptr<ExprAST>> Args)
+    : Callee(Callee), Args(std::move(Args)) {}
+};
+
+
+/// PrototypeAST - This class represents the "prototype" for a function,
+/// which captures its name, and its argument names (thus implicitly the number
+/// of arguments the function takes).
+class PrototypeAST {
+  std::string Name;
+  std::vector<std::string> Args;
+
+public:
+  PrototypeAST(const std::string &name, std::vector<std::string> Args)
+    : Name(name), Args(std::move(Args)) {}
+
+  const std::string &getName() const { return Name; }
+};
+
+/// FunctionAST - This class represents a function definition itself.
+class FunctionAST {
+  std::unique_ptr<PrototypeAST> Proto;
+  std::unique_ptr<ExprAST> Body;
+
+public:
+  FunctionAST(std::unique_ptr<PrototypeAST> Proto,
+              std::unique_ptr<ExprAST> Body)
+    : Proto(std::move(Proto)), Body(std::move(Body)) {}
+};
+
+int main() {
+
 }
